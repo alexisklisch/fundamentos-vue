16e0882dbd923fb675f817100ca0e50eca62e085
Alexis Klisch <unbosquense@gmail.com>
2022-11-25 20:35:38 -0300

    Interpolación de datos
    Aprendo a manejar info que cambia (estado)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-Vue.createApp({
+// vm es la convensión de View Model
+const vm = Vue.createApp({
   data() {
     return {
-      text: 'Hola a todos'
+      text: 'Hola a todos',
+      elementHtml: '<h2>Soy Alexis</h2>'
     }
-  }
+  },
+  // Podemos generar html desde JS
+  // Aquí habilitamos por ejemplo las directivas de Vue
+  template: `<h1 v-text="text"></h1>
+  <div v-html="elementHtml"></div>
+  <p>Hola mi rey</p>
+  `
 }).mount('#app')
+
+/*
+v-text permite agregar texto al elemento sin escribirlo dentro de la etiqueta
+v-once renderiza una sola vez. Si lo alteramos post ejecución, no cambiará
+v-html da la instrucción de leer el string como HTML, sino lo lee como plano
+*/
